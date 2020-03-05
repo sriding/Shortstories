f2d815cd72be219bd6cc9a73c4b9e66ff77c4871
Stephen Riding <ridingjr@gmail.com>
2020-03-04 16:14:18 -0800

    modified profile controller so that it can return tailored database information depending on the id in the url. modified profile and story views.

--- shortstories/Controllers/ProfileController.cs
+++ shortstories/Controllers/ProfileController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shortstories.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,37 @@ namespace shortstories.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly ShortstoriesContext _context;
+
+        public ProfileController(ShortstoriesContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("{controller}/{action}/{id:regex(^[[a-zA-Z0-9_]]*$)}")]
+        public async Task<IActionResult> GetProfile([FromRoute] string id)
+        {
+            try
+            {
+                var profile = await _context.Profile.SingleAsync(o => o.ProfileUsername == id);
+
+                if (profile == null)
+                {
+                    return Redirect("/");
+                }
+
+                return View("~/Views/Profile/Index.cshtml", profile);
+            }
+            catch
+            {
+                return Redirect("/");
+            }
         }
     }
 }
